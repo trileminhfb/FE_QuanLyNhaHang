0135fe5d8004575142327ca312060d8713fc6909
trileminhfb <leminhtrisuper@gmail.com>
2025-04-04 16:12:08 +0700

    feat: update layout admin

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router' // Import router đúng cách
-import './style.css' 
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./style.css";
+
+import Default from "./layout/admin/index.vue";
+// import Default from "./layout/client/index.vue";
+// import Admin from "./layout/admin/index.vue";
 
 const app = createApp(App);
-app.use(router);     
-app.mount('#app');
+
+app.use(router);
+app.component("default-layout", Default);
+// app.component("admin-layout", Admin);
+
+app.mount("#app");
